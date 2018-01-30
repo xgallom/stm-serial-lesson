af0c436ae5458234c18f0ead82e594f78395d159
Milan Gallo <gallo.milan.jr@gmail.com>
2018-01-30 22:23:26 +0100

    Reset to default project
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

--- src/main.c
+++ src/main.c
@@ -14,40 +14,5 @@
 
 int main(void)
 {
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
-
-	GPIO_InitTypeDef gpioInitTd;
-	gpioInitTd.GPIO_Mode = GPIO_Mode_OUT;
-	gpioInitTd.GPIO_OType = GPIO_OType_PP;
-	gpioInitTd.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
-
-	GPIO_Init(GPIOB, &gpioInitTd);
-
-	GPIO_SetBits(GPIOB, GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7);
-
-	static const uint8_t lastIndex = 2;
-	static const uint32_t delaySingle = 100000;
-
-	uint8_t delays[] = { 3, 1 };
-	// Kafka nema zivotne poistenie
-	uint8_t index = 0, isActive = 1;
-
-	for(;;) {
-		if(isActive) {
-			isActive = 0;
-			GPIO_ResetBits(GPIOB, GPIO_Pin_6 | GPIO_Pin_7);
-
-			for(uint32_t time = 0; time < delays[index] * delaySingle; time++) {}
-		}
-		else {
-			isActive = 1;
-			GPIO_SetBits(GPIOB, GPIO_Pin_6 | GPIO_Pin_7);
-
-			for(uint32_t time = 0; time < delays[index] * delaySingle; time++) {}
-
-			index++;
-			if(index == lastIndex)
-				index = 0;
-		}
-	}
+	for(;;);
 }
